381435fec2cef0e4c87cd1ace358367b2460b9ee
rabadin <rabadin@cisco.com>
2023-07-25 17:52:42 +0200

    fix: download to target directory to avoid problems with cross-device renames
    Fixes #3.
    
    Drive-by:
    - Add makefile target to upgrade dependencies.
    - Upgrade all dependencies.

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.20
 
 require (
 	github.com/carlmjohnson/requests v0.23.4
-	github.com/pelletier/go-toml/v2 v2.0.8
+	github.com/pelletier/go-toml/v2 v2.0.9
 	github.com/rs/zerolog v1.29.1
 	github.com/spf13/cobra v1.7.0
-	github.com/stretchr/testify v1.8.3
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
@@ -18,8 +18,8 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/resource.go
+++ internal/resource.go
@@ -5,6 +5,8 @@ package internal
 
 import (
 	"context"
+	"crypto/sha256"
+	b64 "encoding/base64"
 	"fmt"
 	"log"
 	"net/url"
@@ -29,7 +31,7 @@ func NewResourceFromUrl(urls []string, algo string, tags []string, filename stri
 	}
 	url := urls[0]
 	ctx := context.Background()
-	path, err := GetUrl(url, ctx)
+	path, err := GetUrltoTempFile(url, ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get url: %s", err)
 	}
@@ -41,24 +43,39 @@ func NewResourceFromUrl(urls []string, algo string, tags []string, filename stri
 	return &Resource{Urls: urls, Integrity: integrity, Tags: tags, Filename: filename}, nil
 }
 
-// GetUrl downloads the given resource and returns the path to it.
-func GetUrl(u string, ctx context.Context) (string, error) {
+// getUrl downloads the given resource and returns the path to it.
+func getUrl(u string, fileName string, ctx context.Context) (string, error) {
 	_, err := url.Parse(u)
 	if err != nil {
 		return "", fmt.Errorf("invalid url '%s': %s", u, err)
 	}
-	file, err := os.CreateTemp("", "prefix")
-	if err != nil {
-		log.Fatal(err)
-	}
 	err = requests.
 		URL(u).
-		ToFile(file.Name()).
+		ToFile(fileName).
 		Fetch(ctx)
 	if err != nil {
 		return "", fmt.Errorf("failed to download '%s': %s", u, err)
 	}
-	return file.Name(), nil
+	return fileName, nil
+}
+
+// GetUrlToDir downloads the given resource to the given directory and returns the path to it.
+func GetUrlToDir(u string, targetDir string, ctx context.Context) (string, error) {
+	// create temporary name in the target directory.
+	h := sha256.New()
+	h.Write([]byte(u))
+	fileName := filepath.Join(targetDir, fmt.Sprintf(".%s", b64.StdEncoding.EncodeToString(h.Sum(nil))))
+	return getUrl(u, fileName, ctx)
+}
+
+// GetUrlWithDir downloads the given resource to a temporary file and returns the path to it.
+func GetUrltoTempFile(u string, ctx context.Context) (string, error) {
+	file, err := os.CreateTemp("", "prefix")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileName := file.Name()
+	return getUrl(u, fileName, ctx)
 }
 
 func (l *Resource) Download(dir string, ctx context.Context) error {
@@ -68,7 +85,9 @@ func (l *Resource) Download(dir string, ctx context.Context) error {
 		return err
 	}
 	for _, u := range l.Urls {
-		lpath, err := GetUrl(u, ctx)
+		// Download file in the target directory so that the call to
+		// os.Rename is atomic.
+		lpath, err := GetUrlToDir(u, dir, ctx)
 		if err != nil {
 			break
 		}
